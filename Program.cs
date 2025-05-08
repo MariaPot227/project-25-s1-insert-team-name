d0aabe5636c7e64663cfe9d52127e45dc43132e6
Logan Lambert (1000123473) <LAMBLK1@student.op.ac.nz>
2025-05-08 16:44:14 +1200

    Added intro text to program file.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,115 @@
 ﻿using System;
-
+using System.Threading;
 namespace TeamCSFile
 {
     internal class Program
     {
         static void Main()
         {
-            
+            Console.Write("You have arrived at K-Mart with a shopping list containing 4 items:\n\n1. Camping Stove\n\n2. RGB Keyboard\n\n3. Dress\n\n4. Skibidi Toilet Figure");
+
+            Console.Read();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 

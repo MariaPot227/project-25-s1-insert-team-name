d51010f5d0301f524ba6a149d7db7d61e6fd7497
Karl Young (08005603) <YOUNGKG1@student.op.ac.nz>
2025-05-13 12:51:10 +1200

    Added parts of pesudocode

--- Program.cs
+++ Program.cs
@@ -68,8 +68,14 @@ namespace TeamCSFile
         {
 
 
-
-
+            // Needs a boss: 
+            // Enemies: 
+            // Electronics related riddles 
+            // Descriptions of areas and whats around as you go 
+            // different options on what to do in areas or riddles 
+            // Items to pick up or skip 
+            // Extra pop-up quest to help someone adds for good ending or gives bonus loot
+            
 
 
 

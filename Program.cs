c618d09aa82e51f2b3982eb7e8a4fdcf7f21212f
unknown <YOUNGKG1@student.op.ac.nz>
2025-05-14 19:50:47 +1200

    Made the hub menu work mostly

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO.Pipes;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 namespace TeamCSFile
 {
@@ -7,8 +8,8 @@ namespace TeamCSFile
     {
 
 
-        public static int[] InventoryAmount = {0, 0, 0, 0};
-        public static string[] InventoryName = {"Item 1", "Item 2", "Item 3", "Item 4"};
+        public static int[] InventoryAmount = { 0, 0, 0, 0 };
+        public static string[] InventoryName = { "Item 1", "Item 2", "Item 3", "Item 4" };
 
 
         //Global Combat Variables
@@ -23,7 +24,7 @@ namespace TeamCSFile
         {
             Console.WriteLine("You have entered K-mart and you need to collect a few items");
             Hub();
-            
+
 
 
 
@@ -38,7 +39,7 @@ namespace TeamCSFile
 
 
 
-        public static void Hub()
+        static void Hub()
         {
 
             // move rooms - to which room new method
@@ -50,7 +51,7 @@ namespace TeamCSFile
 
             int num = 0;
             bool inMenu = true;
-            
+
             do
             {
                 Console.Clear();
@@ -59,7 +60,7 @@ namespace TeamCSFile
                 Console.WriteLine("1  Rest\n" +
                                   "2  Room Selection\n" +
                                   "3  Check Your inventory\n" +
-                                  "4  Show rooms completed\n" +
+                                  "4  Show areas completed\n" +
                                   "5  Shopping list\n" +
                                   "0  Exit The Menu");
 
@@ -78,19 +79,23 @@ namespace TeamCSFile
                 switch (num)
                 {
                     case 1:
-                        //Task1();
+                        //Rest(); // replenshes HP?
                         break;
 
                     case 2:
-                        //Task2();
+                        RoomSelection();
                         break;
 
                     case 3:
-                        //Task3();
+                        Inventory();
                         break;
 
                     case 4:
-                        //Task4();
+                        //AreasCompleted();
+                        break;
+
+                    case 5:
+                        //ShoppingList();
                         break;
 
                     case 0:
@@ -106,37 +111,78 @@ namespace TeamCSFile
 
             } while (inMenu);
 
+            static void Exit()
+            {
+                Console.WriteLine("Please press any key to exit");
+                Console.ReadLine();
+            }
             string RoomSelection()
             {
+                int num = 0;
+                bool inMenu = true;
+                
+
+                Console.WriteLine("Main Menu\n" + "Please select from the numbers below\n");
+                Console.WriteLine("1  Clothign\n" +
+                                  "2  Electronics\n" +
+                                  "3  Toys\n" +
+                                  "4  Camping\n" +
+                                  "0  Exit The Menu");
+
+                
+
+
+                try
+                {
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Huh");
+                    Console.ReadLine();
+                }
+
                 switch (num)
                 {
                     case 1:
-                        //Task1();
-                        break;
+                        Clothing();
+                        Console.WriteLine("This is Clothing\n" + "Press 0 to exit");
+                        return "Clothing";
+
 
                     case 2:
-                        //Task2();
-                        break;
+                        Electronics();
+                        Console.WriteLine("This is Electronics\n" + "Press 0 to exit");
+                        return "Electronics";
 
                     case 3:
-                        //Task3();
-                        break;
+                        Toys();
+                        Console.WriteLine("This is Toys\n" + "Press 0 to exit");
+                        return "Toys";
 
                     case 4:
-                        //Task4();
-                        break;
+                        Camping();
+                        Console.WriteLine("This is Camping\n" + "Press 0 to exit");
+                        return "Camping";
 
                     case 0:
-                        //Exit();
+                        //Console.WriteLine("This is task 1\n" + "Press 0 to exit");
+
+                        //return to menu;
                         inMenu = false;
-                        break;
-                    
+                        return "Exiting";
+                    default:
+                        Console.WriteLine("Invalid Input. Please keep it between 0-4");
+                        Console.ReadLine();
+                        return "Invalid";
+
                 }
 
 
 
-        }
+            }
 
+        }
 
 
 
@@ -160,9 +206,9 @@ namespace TeamCSFile
 
 
         static void Combat()
-            {
+        {
 
-                string[] Room2Enemies = {"TestEnemy1", "TestEnemy2", "TestEnemy3", "TestEnemy4", "TestEnemy5"};
+            string[] Room2Enemies = { "TestEnemy1", "TestEnemy2", "TestEnemy3", "TestEnemy4", "TestEnemy5" };
 
 
 
@@ -268,96 +314,28 @@ namespace TeamCSFile
 
             int enemies = 0;        // < number of enemies in battle
 
-            string[] onfield = {"null", "null", "null"};    // < array of the enemies that are on the field, will be populated by pulling from room's array of possible enemies
+            string[] onfield = { "null", "null", "null" };    // < array of the enemies that are on the field, will be populated by pulling from room's array of possible enemies
 
             Random rand = new Random();
 
             enemies = rand.Next(1, 4);  // < generates number of enemies that will appear in the battle, from 1 - 3 (4 exclusive)
 
-               for (int i = 0; i < enemies; i++)     // will loop as many times as there are enemies in the battle as decided by rng earlier (1-3 ^)
+            for (int i = 0; i < enemies; i++)     // will loop as many times as there are enemies in the battle as decided by rng earlier (1-3 ^)
+            {
+                switch (Room)   // < depending on current room
                 {
-                    switch (Room)   // < depending on current room
-                    {
-                        case 2:             // < if player is in Room 2 (Camping Section)
+                    case 2:             // < if player is in Room 2 (Camping Section)
 
-                            int PullEnemy = rand.Next(Room2Enemies.Length);     // < generates random number to pick random enemy from available
+                        int PullEnemy = rand.Next(Room2Enemies.Length);     // < generates random number to pick random enemy from available
 
-                            onfield[i] = Room2Enemies[PullEnemy];               // < assigns a random enemy as decided from the previous line to the current field slot
-
-                            break;
-
-                        default:
-                            break;
-
-                    }
-
-
-
-
-
-                }
-
-
-
-
-                // v Combat UI
-
-                switch (enemies)                        // < changes starting text depending on number of enemies
-                {
-
-                    case 1:                            // < if there is only one enemy
-
-                        Console.WriteLine($"\n\nA wild {onfield[0]} approaches!");  // < tells user what enemy they are facing
+                        onfield[i] = Room2Enemies[PullEnemy];               // < assigns a random enemy as decided from the previous line to the current field slot
 
                         break;
-
-                    case 2:
-
-                        Console.WriteLine($"\n\nYou are stopped by {onfield[0]} and {onfield[1]}.");
-
-                        break;
-
-                    case 3:
-
-                        Console.WriteLine($"\n\nYou run into {onfield[0]}, {onfield[1]} and {onfield[2]}");
-
-                        break;
-
 
                     default:
-                                    // < for boss battles?
                         break;
 
-
-
-
                 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -368,8 +346,76 @@ namespace TeamCSFile
 
 
 
+            // v Combat UI
 
-            static void Clothing()
+            switch (enemies)                        // < changes starting text depending on number of enemies
+            {
+
+                case 1:                            // < if there is only one enemy
+
+                    Console.WriteLine($"\n\nA wild {onfield[0]} approaches!");  // < tells user what enemy they are facing
+
+                    break;
+
+                case 2:
+
+                    Console.WriteLine($"\n\nYou are stopped by {onfield[0]} and {onfield[1]}.");
+
+                    break;
+
+                case 3:
+
+                    Console.WriteLine($"\n\nYou run into {onfield[0]}, {onfield[1]} and {onfield[2]}");
+
+                    break;
+
+
+                default:
+                    // < for boss battles?
+                    break;
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+        static void Clothing()
         {
 
 
@@ -413,7 +459,7 @@ namespace TeamCSFile
 
 
 
-        
+
         static void Electronics()
         {
 
@@ -425,11 +471,14 @@ namespace TeamCSFile
             // different options on what to do in areas or riddles 
             // Items to pick up or skip 
             // Extra pop-up quest to help someone adds for good ending or gives bonus loot
-            
+
 
 
 
         }
+
+
+
 
 
 
@@ -478,8 +527,7 @@ namespace TeamCSFile
 
 
     }
-
-
 }
-    
+
+
 
